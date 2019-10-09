2875a5f6887a467581eae3d19a809ab82d4754a3
Dan <cornul11@gmail.com>
2019-10-09 04:48:41 +0200

    added basic model

---
+++ Songregator/api/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+# Create your models here.
+class Song(models.Model):
+    artist_familiarity = models.FloatField()
+    artist_hotttnesss = models.FloatField()
+    artist_id = models.CharField(max_length=80)
+    artist_latitude = models.FloatField()
+    artist_location = models.IntegerField()
+    artist_longitude = models.FloatField()
+    artist_name = models.CharField(max_length=80)
+    artist_similar = models.FloatField()
+    artist_terms = models.CharField(max_length=80)
+    artist_terms_freq = models.FloatField()
+    release_id = models.IntegerField()
+    release_name = models.IntegerField()
+    song_artist_mbtags = models.FloatField()
+    song_artist_mbtags_count = models.FloatField()
+    song_bars_confidence = models.FloatField()
+    song_bars_start = models.FloatField()
+    song_beats_confidence = models.FloatField()
+    song_beats_start = models.FloatField()
+    song_duration = models.FloatField()
+    song_end_of_fade_in = models.FloatField()
+    song_hotttnesss = models.FloatField()
+    song_id = models.CharField(max_length=80)
+    song_key = models.FloatField()
+    song_key_confidence = models.FloatField()
+    song_loudness = models.FloatField()
+    song_mode = models.IntegerField()
+    song_mode_confidence = models.FloatField()
+    song_start_of_fade_out = models.FloatField()
+    song_tatums_confidence = models.FloatField()
+    song_tatums_start = models.FloatField()
+    song_tempo = models.FloatField()
+    song_time_signature = models.FloatField()
+    song_time_signature_confidence = models.FloatField()
+    song_title = models.IntegerField()
+    song_year = models.IntegerField()
+
+    def __str__(self):
+        return str(self.song_title)
