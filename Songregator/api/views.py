1f605eb6072e412eb714197af49eb7538b9d2346
Dan <cornul11@gmail.com>
2019-10-09 04:59:27 +0200

    added basic endpoint

--- Songregator/api/views.py
+++ Songregator/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import SongSerializer
+from .models import Song
+
 
 # Create your views here.
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all().order_by('artist_longitude')
+    serializer_class = SongSerializer
