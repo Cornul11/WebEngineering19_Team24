d63e84fbb5396768ce34e086d79c283e10b5ff61
Dan <cornul11@gmail.com>
2019-10-19 01:10:32 +0200

    site looks overhaul

--- Songregator/frontend/src/App.js
+++ Songregator/frontend/src/App.js
@@ -5,26 +5,37 @@ import ArtistsList from "./ArtistsList";
 import SongsList from "./SongsList";
 // import logo from './logo.svg';
 import './App.css';
+import SearchBar from "./SongForm";
 
 
 const BaseLayout = () => (
-    <div className="container-fluid">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Django React Demo</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link" href="/">Artists</a>
-                    <a className="nav-item nav-link" href="/songs">Songs</a>
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow">
+            <div className="container">
+                <a className="navbar-brand" href="/">Songregator</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a class="nav-link" href="/">Artists</a>
+                        </li>
+                        <li className="nav-item active">
+                            <a class="nav-link" href="/songs">Songs</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
-        <div className="content">
-            <Route path="/" exact component={ArtistsList}/>
-            <Route path="/songs" exact component={SongsList}/>
+        <div className="container">
+            <div className="card border-0 shadow my-5">
+                <div className="card-body p-5">
+                    <Route path="/" exact component={ArtistsList}/>
+                    <Route path="/songs" exact component={SearchBar}/>
+                </div>
+            </div>
         </div>
     </div>
 );
