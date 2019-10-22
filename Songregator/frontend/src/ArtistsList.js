cf1b108ae53afeded1abb1a465053d35dc4da85c
Dan <cornul11@gmail.com>
2019-10-23 00:02:19 +0200

    added popup window

--- Songregator/frontend/src/ArtistsList.js
+++ Songregator/frontend/src/ArtistsList.js
@@ -1,70 +1,124 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from "react";
 import ArtistsService from "./ArtistsService";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const artistsService = new ArtistsService();
 
 class ArtistsList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            artists: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      artists: [],
+    };
+  }
 
-    componentDidMount() {
-        let self = this;
-        artistsService.getArtists().then(function (result) {
-            console.log(result);
-            self.setState({artists: result})
-        });
-    }
+  componentDidMount() {
+    let self = this;
+    artistsService.getArtists().then(function(result) {
+      console.log(result);
+      self.setState({artists: result});
+    });
+  }
 
-    handleDelete(e, artist_name) {
-        let self = this;
-        artistsService.deleteArtist({artist_name: artist_name}).then(() => {
-            const newArr = self.state.artists.filter(function (obj) {
-                return obj.artist_name !== artist_name;
-            });
-            self.setState({artists : newArr})
-        });
-    }
-    render() {
-        let tdStyle = {width: '15%'};
-        return (
-            <div>
-                <p>Debug: {this.state.artists.length} items</p>
-                <h1 className="font-weight-light">Artists list</h1>
-                <table className="table table-striped table-hover">
-                    <thead className="thead-dark" key="thead">
-                    <tr>
-                        <th>id</th>
-                        <th>Name</th>
-                        <th>Terms</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.artists.map(a =>
-                        <tr key={a.artist_name}>
-                            <td>{a.artist_id}  </td>
-                            <td>{a.artist_name}</td>
-                            <td>{a.artist_terms}</td>
-                            <td style={tdStyle}>
-                                <div className="btn-group" role="group">
-                                    <button type="button" className="btn btn-danger btn-sm"
-                                            onClick={(e) => this.handleDelete(e, a.artist_name)}> Delete
-                                    </button>
-                                    <a role="button" className="btn btn-secondary btn-sm"
-                                       href={"/customer/" + a.pk}> Update</a>
-                                </div>
-                            </td>
-                        </tr>)}
-                    </tbody>
-                </table>
-                {/*<button className="btn btn-primary" onClick={this.nextPage}>Next</button>*/}
-            </div>
-        );
-    }
+  handleDelete(e, artist_name) {
+    let self = this;
+    artistsService.deleteArtist({artist_name: artist_name}).then(() => {
+      const newArr = self.state.artists.filter(function(obj) {
+        return obj.artist_name !== artist_name;
+      });
+      self.setState({artists: newArr});
+    });
+  }
+
+  render() {
+    let tdStyle = {width: "15%"};
+    return (
+      <div>
+        <PopupEditingWindow artists={this.state.artists} />
+        <p>Debug: {this.state.artists.length} items</p>
+        <h1 className="font-weight-light">Artists list</h1>
+        <table className="table table-striped table-hover">
+          <thead className="thead-dark" key="thead">
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Terms</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.artists.map(a => (
+              <tr key={a.artist_name}>
+                <td>{a.artist_id} </td>
+                <td>{a.artist_name}</td>
+                <td>{a.artist_terms}</td>
+                <td style={tdStyle}>
+                  <div className="btn-group" role="group">
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-sm"
+                      onClick={e => this.handleDelete(e, a.artist_name)}
+                    >
+                      {" "}
+                      Delete
+                    </button>
+                    <a
+                      role="button"
+                      className="btn btn-secondary btn-sm"
+                      href={"/customer/" + a.pk}
+                    >
+                      {" "}
+                      Update
+                    </a>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/*<button className="btn btn-primary" onClick={this.nextPage}>Next</button>*/}
+      </div>
+    );
+  }
+}
+
+function PopupEditingWindow(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Open testing popup
+      </Button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Edit artist?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {props.artists.map(a => (
+            <p>{a.artist_name}</p>
+          ))}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default ArtistsList;
--- Songregator/frontend/src/SongsList.js
+++ Songregator/frontend/src/SongsList.js
@@ -1,52 +1,53 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import SongsService from "./SongsService";
 
 const songsService = new SongsService();
 
 class SongsList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            songs: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: [],
+    };
+  }
 
-    componentDidMount() {
-        var self = this;
-        songsService.getSongs().then(function (result) {
-            console.log(result);
-            self.setState({songs: result})
-        });
-    }
+  componentDidMount() {
+    let self = this;
+    songsService.getSongs().then(function(result) {
+      console.log(result);
+      self.setState({songs: result});
+    });
+  }
 
-    render() {
-        return (
-            <div className="customers--list">
-                <table className="table">
-                    <thead key="thead">
-                    <tr>
-                        <th>id</th>
-                        <th>Name</th>
-                        <th>Genre</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.songs.map(a =>
-                        <tr key={a.song_id}>
-                            <td>{a.song_id}  </td>
-                            <td>{a.song_title}</td>
-                            <td>{a.artist_id}</td>
-                            {/*<td>*/}
-                            {/*    <button onClick={(e) => this.handleDelete(e, a.pk)}> Delete</button>*/}
-                            {/*    <a href={"/customer/" + a.pk}> Update</a>*/}
-                            {/*</td>*/}
-                        </tr>)}
-                    </tbody>
-                </table>
-                {/*<button className="btn btn-primary" onClick={this.nextPage}>Next</button>*/}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="customers--list">
+        <table className="table">
+          <thead key="thead">
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.songs.map(a => (
+              <tr key={a.song_id}>
+                <td>{a.song_id} </td>
+                <td>{a.song_title}</td>
+                <td>{a.artist_id}</td>
+                {/*<td>*/}
+                {/*    <button onClick={(e) => this.handleDelete(e, a.pk)}> Delete</button>*/}
+                {/*    <a href={"/customer/" + a.pk}> Update</a>*/}
+                {/*</td>*/}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/*<button className="btn btn-primary" onClick={this.nextPage}>Next</button>*/}
+      </div>
+    );
+  }
 }
 
 export default SongsList;
