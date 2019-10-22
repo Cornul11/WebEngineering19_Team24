c2302d21739a57f015d4bcc65dbdfe744a8cf3ef
Dan <cornul11@gmail.com>
2019-10-23 00:13:29 +0200

    fixed warnings

--- Songregator/frontend/src/ArtistsService.js
+++ Songregator/frontend/src/ArtistsService.js
@@ -1,16 +1,14 @@
-import axios from 'axios';
-const API_URL = 'http://localhost:8000';
+import axios from "axios";
+const API_URL = "http://localhost:8000";
 
 export default class ArtistsService {
-    constructor() {}
+  getArtists() {
+    const url = `${API_URL}/artists?timestamp=${new Date().getTime()}`;
+    return axios.get(url).then(response => response.data);
+  }
 
-    getArtists() {
-        const url = `${API_URL}/artists?timestamp=${new Date().getTime()}`;
-        return axios.get(url).then(response => response.data);
-    }
-
-    deleteArtist(artist) {
-        const url = `${API_URL}/songs/delete/${artist.artist_name}/`;
-        return axios.delete(url);
-    }
+  deleteArtist(artist) {
+    const url = `${API_URL}/songs/delete/${artist.artist_name}/`;
+    return axios.delete(url);
+  }
 }
--- Songregator/frontend/src/SongsService.js
+++ Songregator/frontend/src/SongsService.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
-const API_URL = 'http://localhost:8000';
+import axios from "axios";
+const API_URL = "http://localhost:8000";
 
 export default class SongsService {
-    constructor() {}
-
-    getSongs() {
-        const url = `${API_URL}/songs`; // TODO: to change for name
-        return axios.get(url).then(response => response.data);
-    }
+  getSongs() {
+    const url = `${API_URL}/songs`;
+    return axios.get(url).then(response => response.data);
+  }
 }
